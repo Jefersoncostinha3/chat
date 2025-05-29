cdd2d5950360b8d5f06ed426722058f9f7ebbf17
Jefersoncostinha3 <derrubadoresdepagina3@gmail.com>
2025-05-29 00:18:57 -0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,37 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true // Adiciona createdAt e updatedAt automaticamente
+});
+
+// Middleware para criptografar a senha antes de salvar
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Método para comparar a senha fornecida com a senha criptografada
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
